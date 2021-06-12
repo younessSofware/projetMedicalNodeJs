c08ae9685b8be48542213fd057c44d6f7a207335
younessSofware <youness3.toyouness18@gmail.com>
2021-06-12 16:49:02 +0100

    euro 2020

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express  = require('express');
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use( express.static( "uploads" ) );
 
 app.get('/', (req, res) => {
-    res.send("Hello my friend")
-})
+    res.render('index', {})
+});
 app.listen(process.env.PORT || 5000, () => {
     console.log('lesten port 5000');
 })
